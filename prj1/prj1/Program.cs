3d888dfc0f4fa0440acbb11d4e1b387a54c7c143
hoangtrang0610 <69054102+hoangtrang0610@users.noreply.github.com>
2020-12-23 15:34:15 +0700

    đổi tên

--- prj1/prj1/Program.cs
+++ prj1/prj1/Program.cs
@@ -6,7 +6,7 @@ namespace prj1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello abc!");
         }
     }
 }
